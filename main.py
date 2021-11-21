7bd19c768df91f5523367874fb04b02db372170b
voldlorte <37194041+voldlorte@users.noreply.github.com>
2021-11-21 18:00:47 +0100

    Added background
    added a bg, title, icon and a few adjustments

--- main.py
+++ main.py
@@ -5,12 +5,44 @@ import pygame
 pygame.init()
 
 # Adding a screen
-screen = pygame.display.set_mode((900, 900))
+screen = pygame.display.set_mode((600, 600))
+
+# Adding a title for the game
+pygame.display.set_caption("Sudoku", "Sudoku")
+
+# Loading The icon and the background
+icon = pygame.image.load("sudokuIcon.png")
+backGround = pygame.image.load("sudokuGrid.jpg")
+coordinates = [0, 0]
+
+# Displaying the icon
+pygame.display.set_icon(icon)
 
 running = True
 
 # Starting the game loop
 while running:
+
+    # Filling the BG with a color
+    screen.fill((0, 0, 0))
+
+    # Displaying the background
+    screen.blit(backGround, coordinates)
+
+    # We loop through the events
     for event in pygame.event.get():
+
+        # Check if we pressed the quit button
         if event.type == pygame.QUIT:
             running = False
+
+        # Check if we pressed the left mouse button
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.BUTTON_LEFT:
+                print("left pressed")
+
+    # Debugging
+    screen.blit(pygame.font.Font.render(pygame.font.SysFont("arial", 32), "0", True, (0, 0, 0)), (300, 300))
+
+    # Updating the game
+    pygame.display.update()
