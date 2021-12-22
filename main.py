d11cb9eb053af598cbcc076f585e0f3bd844bbb5
voldlorte <37194041+voldlorte@users.noreply.github.com>
2021-12-22 01:20:36 +0100

    simple changes.

--- main.py
+++ main.py
@@ -20,13 +20,12 @@ coordinates = [0, 0]
 pygame.display.set_icon(icon)
 
 
-def show_random(num_list):
-    # font1 = pygame.font.Font.render(pygame.font.SysFont("arial", 26), str(random.randrange(10)), True, (0, 0, 0))
+def render_random(num_list):
     f = 18
-    for i in num_list:
+    for row in range(len(num_list)):
         s = 28
-        for j in i:
-            print(f'{screen.blit(pygame.font.Font.render(pygame.font.SysFont("arial", 26), str(num_list[i][j]), True, (0, 0, 0)), (s, f))}')
+        for column in range(len(num_list)):
+            screen.blit(pygame.font.Font.render(pygame.font.SysFont("arial", 26), str(num_list[row][column]), True, (0, 0, 0)), (s, f))
             s += 66
         f += 66
 
@@ -46,7 +45,7 @@ def set_random_indexes(empty_cells):
                 continue
             else:
                 ran_list[j] = random.randrange(1, 10)
-    print("random indexes", ran_list)
+    # print("random indexes", ran_list)
     return ran_list
 
 
@@ -64,7 +63,10 @@ while starting:
     for row in range(9):
         n_list = []
         for column in range(9):
-            n_list.append(random.randrange(1, 9))
+            n_random = random.randrange(1, 10)
+            n_list.append(n_random)
+            while n_list.count(n_list[column]) > 1:
+                n_list[column] = random.randrange(1, 10)
         my_list.append(n_list)
 
     # extracting tables from the matrix
@@ -99,17 +101,33 @@ while starting:
                 if 6 <= row <= 8 and 6 <= column <= 8:
                     tables[8].append(my_list[row][column])
 
+    # making sure the numbers don't repeat in both main rows and tables.
+    for row in range(9):
+        for column in range(9):
+            while tables[row].count(tables[row][column]) > 1:
+                tables[row][column] = random.randrange(1, 10)
+                while my_list[row].count(my_list[row][column]) > 1:
+                    my_list[row][column] = random.randrange(1, 10)
+
     # indexes of the blank cells.
     for k in range(len(tables)):
-        random_ind = set_random_indexes(5)
+        random_ind = set_random_indexes(4)
         for i in range(1, len(tables[k]) + 1):
             for j in range(len(random_ind)):
                 if i == random_ind[j]:
                     # we leave this blank, the user should do an input.
                     tables[k][i - 1] = 11
 
-    print(my_list)
-    print(tables)
+    # final_list = []
+    # for i in range(9):
+    #     final_list_content = []
+    #     for j in range(9):
+
+
+    render_random(tables)
+
+    print("main list", my_list)
+    print("tables", tables)
 
     pygame.display.update()
     starting = False
